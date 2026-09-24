26551fa214510abaae6b9d5675238b5741adf183
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2026-09-24 14:37:21 +0330

    feat: bundle self-hosted Vazirmatn font with @fontsource/vazirmatn

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,9 @@
+import "@fontsource/vazirmatn/300.css";
+import "@fontsource/vazirmatn/400.css";
+import "@fontsource/vazirmatn/500.css";
+import "@fontsource/vazirmatn/600.css";
+import "@fontsource/vazirmatn/700.css";
+import "@fontsource/vazirmatn/800.css";
 import { App } from "@/App.tsx";
 import "@/index.css";
 import { registerServiceWorker } from "@/lib/registerSW.ts";
